95c17631ccd6ede77518c87a8fea72a236fc7186
Flavio Roth <flavio.roth@gmail.com>
2020-04-16 11:12:11 +0200

    Don't fail if AppRun doesn't exist yet

--- scripts/copy_libs.sh
+++ scripts/copy_libs.sh
@@ -16,4 +16,4 @@ cp /usr/lib/libitk3.3.so.1 -p AppDir/usr/lib/
 cp /usr/lib/libitk3.3.so -p AppDir/usr/lib/
 cp /usr/lib/libitkstub3.3.a -p AppDir/usr/lib/
 cp /usr/lib/python2.7 -p -r AppDir/usr/lib/
-rm AppDir/AppRun
+rm -f AppDir/AppRun
